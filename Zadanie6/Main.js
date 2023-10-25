9d02fb919d0e1887386e4ea7b3fd1c278e226494
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2023-10-25 22:58:32 +0300

    Update Main.js

--- Zadanie6/Main.js
+++ Zadanie6/Main.js
@@ -1,26 +1,57 @@
 function calculateCost() {
     // Получение значений из формы
-    var quantity = document.getElementById("quantity").value;
-    var product = document.getElementById("product").value;
+    var quantity = parseInt(document.getElementById("quantity").value);
+    var product = document.querySelector('input[name="product"]:checked').value;
+    var option = document.getElementById("option").value;
+    var property = document.getElementById("property").checked;
 
     // Расчет стоимости заказа
-    var price;
+    var price = 0;
     if (product === "product1") {
-        price = 90000; //Цена за Фотоаппарат
+        price = 90000 * quantity; //Цена за Фотоаппарат
     } else if (product === "product2") {
-        price = 15000; // Цена за Камеру
+        price = 15000 * quantity;// Цена за Камеру
+        price += option === "option0" ? 0 : 0;
+        price += option === "option1" ? 5000 : 0;
+        price += option === "option2" ? 3000 : 0;
+        price += option === "option3" ? 1700 : 0;
+        price += option === "option4" ? 7000 : 0;
+        price += option === "option5" ? 4000 : 0;
     } else if (product === "product3") {
-        price = 135000; // Цена за Телефон
+        price = 135000 * quantity;// Цена за Телефон
+        price += property ? 6000 : 0;
     }
     else if (product === "product4") {
-        price = 20000; // Цена за Полароид
-
+        price = 20000 * quantity; // Цена за Полароид
+        price += option === "option0" ? 0 : 0;
+        price += option === "option1" ? 5000 : 0;
+        price += option === "option2" ? 3000 : 0;
+        price += option === "option3" ? 1700 : 0;
+        price += option === "option4" ? 7000 : 0;
+        price += option === "option5" ? 4000 : 0;
+        price += property ? 6000 : 0;
     }
-    var totalCost = quantity * price;
+    else if (product === "product5") {
+        price = 110000 * quantity; // Цена за Полароид
+        price += option === "option0" ? 0 : 0;
+        price += option === "option1" ? 5000 : 0;
+        price += option === "option2" ? 3000 : 0;
+        price += option === "option3" ? 1700 : 0;
+        price += option === "option4" ? 7000 : 0;
+        price += option === "option5" ? 4000 : 0;
+        price += property ? 6000 : 0;
+    }
 
-    // Вывод стоимости заказа на страницу
-    document.getElementById("result").innerHTML = "Стоимость заказа: " + totalCost + " руб.";
+    // Обновление отображения цены
+    document.querySelector(".price h5").textContent = "Цена: " + price + " руб.";
 }
+
+
+
+
+
+
+
 
 
 
